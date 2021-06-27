1d980fccb4de5b5e109b04b4656d7f82dce73647
CRZA <contact@crza.dev>
2021-06-27 14:02:05 +0530

    added button options

--- src/index.ts
+++ src/index.ts
@@ -1,123 +1,136 @@
-import {
-  MessageEmbed,
-  TextChannel,
-  DMChannel,
-  Message,
-  MessageButton,
-  MessageComponentInteraction,
-} from "discord.js";
+import {DMChannel, Message, MessageButton, MessageComponentInteraction, MessageEmbed, TextChannel,} from "discord.js";
+import {ButtonOption} from "./types/ButtonOption";
+
 const availableEmojis = ["⏮️", "◀️", "⏹️", "▶️", "⏭️"];
+
 export class Pagination {
-  private message?: Message;
-  private readonly channel: TextChannel | DMChannel;
-  private readonly pages: MessageEmbed[];
-  private index = 0;
+    private message?: Message;
+    private readonly channel: TextChannel | DMChannel;
+    private readonly pages: MessageEmbed[];
+    private index = 0;
 
-  /**
-   *
-   * @param {TextChannel | DMChannel} channel - The target channel
-   * @param {MessageEmbed[]} pages - Embed pages
-   * @param {string} [footerText] - Optional footer text, will show `Text 1 of 5` if you pass `Text`, for example
-   */
-  constructor(
-    channel: TextChannel | DMChannel,
-    pages: MessageEmbed[],
-    private readonly footerText = "Page"
-  ) {
-    this.channel = channel;
-    this.pages = pages.map((page, pageIndex) => {
-      if (page.footer && (page.footer.text || page.footer.iconURL)) return page;
-      return page.setFooter(
-        `${footerText} ${pageIndex + 1} of ${pages.length}`
-      );
-    });
-  }
-
-  /**
-   * Starts the pagination
-   */
-  async paginate(): Promise<void> {
-    this.message = await this.channel.send({
-      embeds: [this.pages[this.index]],
-      components: [
-        {
-          type: 1,
-          components: availableEmojis.map((x) => {
-            return new MessageButton({
-              emoji: x,
-              style: 1,
-              type: 2,
-              customID: x,
-            });
-          }),
-        },
-      ],
-    });
-    if (this.pages.length < 2) {
-      return;
-    }
-    const interactionCollector =
-      this.message.createMessageComponentInteractionCollector(
-        (_interaction: MessageComponentInteraction) => {
-          return true;
-        },
-        {
-          time: 60000,
-          max: this.pages.length * 5,
+    /**
+     *
+     * @param {TextChannel | DMChannel} channel - The target channel
+     * @param {MessageEmbed[]} pages - Embed pages
+     * @param {string} [footerText] - Optional footer text, will show `Text 1 of 5` if you pass `Text`, for example
+     * @param {ButtonOption[]} options - optional options for the buttons
+     */
+    constructor(
+        channel: TextChannel | DMChannel,
+        pages: MessageEmbed[],
+        private readonly footerText = "Page",
+        private readonly options ?: ButtonOption[]
+    ) {
+        if (options && options.length > 5) {
+            throw new TypeError("You have passed more than 5 buttons as options")
+        } else if (options && options.length < 4) {
+            throw new TypeError("You have passed less than 5 buttons as options")
         }
-      );
-    interactionCollector.on("collect", async (interaction) => {
-      const { customID } = interaction;
-      switch (customID) {
-        case availableEmojis[0]:
-          // Start
-          if (this.index !== 0) {
-            this.index = 0;
-            interaction.update({
-              embeds: [this.pages[this.index]],
-            });
-          }
-          break;
-        case availableEmojis[1]:
-          // Prev
-          this.index--;
-          if (this.index <= 0) this.index = this.pages.length - 1;
-          interaction.update({
+        this.channel = channel;
+        this.pages = pages.map((page, pageIndex) => {
+            if (page.footer && (page.footer.text || page.footer.iconURL)) return page;
+            return page.setFooter(
+                `${footerText} ${pageIndex + 1} of ${pages.length}`
+            );
+        });
+    }
+
+    /**
+     * Starts the pagination
+     */
+    async paginate(): Promise<void> {
+        this.message = await this.channel.send({
             embeds: [this.pages[this.index]],
-          });
-          break;
-        case availableEmojis[2]:
-          // Stop
-          interactionCollector.stop("stopped by user");
-          await interaction.update({
-            components: [],
-          });
-          break;
-        case availableEmojis[3]:
-          // Next
-          this.index++;
-          if (this.index >= this.pages.length) {
-            this.index = 0;
-          }
-          interaction.update({
-            embeds: [this.pages[this.index]],
-          });
-          break;
-        case availableEmojis[4]:
-          // End
-          if (this.index !== this.pages.length - 1) {
-            this.index = this.pages.length - 1;
-            interaction.update({
-              embeds: [this.pages[this.index]],
+            components: [
+                {
+                    type: 1,
+                    components: this.options ? this.options.map((x, i) => {
+                        return new MessageButton(
+                            {
+                                emoji: x.emoji,
+                                style: x.style,
+                                type: 2,
+                                label: x.label,
+                                customID: availableEmojis[i]
+                            }
+                        )
+                    }) : availableEmojis.map((x) => {
+                        return new MessageButton({
+                            emoji: x,
+                            style: 1,
+                            type: 2,
+                            customID: x,
+                        });
+                    }),
+                },
+            ],
+        });
+        if (this.pages.length < 2) {
+            return;
+        }
+        const interactionCollector =
+            this.message.createMessageComponentInteractionCollector(
+                (_interaction: MessageComponentInteraction) => {
+                    return true;
+                },
+                {
+                    time: 60000,
+                    max: this.pages.length * 5,
+                }
+            );
+        interactionCollector.on("collect", async (interaction) => {
+            const {customID} = interaction;
+            switch (customID) {
+                case availableEmojis[0]:
+                    // Start
+                    if (this.index !== 0) {
+                        this.index = 0;
+                        await interaction.update({
+                            embeds: [this.pages[this.index]],
+                        });
+                    }
+                    break;
+                case availableEmojis[1]:
+                    // Prev
+                    this.index--;
+                    if (this.index <= 0) this.index = this.pages.length - 1;
+                    await interaction.update({
+                        embeds: [this.pages[this.index]],
+                    });
+                    break;
+                case availableEmojis[2]:
+                    // Stop
+                    interactionCollector.stop("stopped by user");
+                    await interaction.update({
+                        components: [],
+                    });
+                    break;
+                case availableEmojis[3]:
+                    // Next
+                    this.index++;
+                    if (this.index >= this.pages.length) {
+                        this.index = 0;
+                    }
+                    await interaction.update({
+                        embeds: [this.pages[this.index]],
+                    });
+                    break;
+                case availableEmojis[4]:
+                    // End
+                    if (this.index !== this.pages.length - 1) {
+                        this.index = this.pages.length - 1;
+                        await interaction.update({
+                            embeds: [this.pages[this.index]],
+                        });
+                    }
+                    break;
+            }
+        });
+        interactionCollector.on("end", async () => {
+            await this?.message?.edit({
+                components: [],
             });
-          }
-          break;
-      }
-    });
-    interactionCollector.on("end", async () => {
-      await this?.message?.edit({
-        components: [],
-      });
-    });
-  }
+        });
+    }
 }
