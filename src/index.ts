c43027261e100bd575c7f59540fd51dc15a7187b
CRZA5 <contact@crza.dev>
2022-07-04 22:29:22 +0530

    updated discord.js to 13.8.1 and Closes #1

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
-import {DMChannel, Message, MessageButton, MessageEmbed, TextChannel, Mess, MessageAttachment} from "discord.js";
+import {
+    DMChannel,
+    Message,
+    MessageButton,
+    MessageEmbed,
+    TextChannel,
+    MessageAttachment,
+    UserResolvable
+} from "discord.js";
 import {ButtonOption} from "./types/ButtonOption";
 
 const availableEmojis = ["⏮️", "◀️", "⏹️", "▶️", "⏭️"];
@@ -17,6 +25,8 @@ class Pagination {
      * @param {string} [footerText] - Optional footer text, will show `Text 1 of 5` if you pass `Text`, for example
      * @param {number} timeout - How long button need to be active
      * @param {ButtonOption[]} options - optional options for the buttons
+     * @param {UserResolvable} Author - To limit the pagination to a specific author
+     * @param files {MessageAttachment[]} - Optional files to attach
      */
     constructor(
         channel: TextChannel | DMChannel,
@@ -24,8 +34,9 @@ class Pagination {
         private readonly footerText = "Page",
         private readonly timeout?: number,
         private readonly options ?: ButtonOption[],
+        private readonly Author?: UserResolvable,
         private readonly files ?: MessageAttachment[],
-) {
+    ) {
         if (options && options.length > 5) {
             throw new TypeError("You have passed more than 5 buttons as options")
         } else if (options && options.length < 4) {
@@ -35,7 +46,7 @@ class Pagination {
         if (files) {
             this.files = files;
         }
-        
+
         this.pages = pages.map((page, pageIndex) => {
             if (page.footer && (page.footer.text || page.footer.iconURL)) return page;
             return page.setFooter({
@@ -107,10 +118,15 @@ class Pagination {
         if (this.pages.length < 2) {
             return;
         }
+        const author = this.Author ? this.channel.client.users.resolve(this.Author) : undefined;
         const interactionCollector =
             this.message?.createMessageComponentCollector(
                 {
                     max: this.pages.length * 5,
+                    filter: (x) => {
+                        return !(author && x.user.id !== author.id);
+
+                    }
                 }
             );
         setTimeout(async () => {
@@ -120,34 +136,34 @@ class Pagination {
             });
         }, this.timeout ? this.timeout : 60000)
         interactionCollector.on("collect", async (interaction) => {
-            const { customId } = interaction;
+            const {customId} = interaction;
             let newIndex =
-              customId === availableEmojis[0]
-                ? 0 // Start
-                : customId === availableEmojis[1]
-                ? this.index - 1 // Prev
-                : customId === availableEmojis[2]
-                ? NaN // Stop
-                : customId === availableEmojis[3]
-                ? this.index + 1 // Next
-                : customId === availableEmojis[4]
-                ? this.pages.length - 1 // End
-                : this.index;
+                customId === availableEmojis[0]
+                    ? 0 // Start
+                    : customId === availableEmojis[1]
+                        ? this.index - 1 // Prev
+                        : customId === availableEmojis[2]
+                            ? NaN // Stop
+                            : customId === availableEmojis[3]
+                                ? this.index + 1 // Next
+                                : customId === availableEmojis[4]
+                                    ? this.pages.length - 1 // End
+                                    : this.index;
             if (isNaN(newIndex)) {
-              // Stop
-              interactionCollector.stop("stopped by user");
-              await interaction.update({
-                components: [],
-              });
+                // Stop
+                interactionCollector.stop("stopped by user");
+                await interaction.update({
+                    components: [],
+                });
             } else {
-              if (newIndex < 0) newIndex = 0;
-              if (newIndex >= this.pages.length)
-                  newIndex = this.pages.length - 1;
-              this.index = newIndex;
-              await interaction.update({
-                  embeds: [this.pages[this.index]],
-                  ...(this.files && {files: [this.files[this.files]]})
-              });
+                if (newIndex < 0) newIndex = 0;
+                if (newIndex >= this.pages.length)
+                    newIndex = this.pages.length - 1;
+                this.index = newIndex;
+                await interaction.update({
+                    embeds: [this.pages[this.index]],
+                    ...(this.files && {files: [this.files[this.index]]}),
+                });
             }
         });
         interactionCollector.on("end", async () => {
